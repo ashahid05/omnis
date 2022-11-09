13c65c4b7af77aa43b0b991b16fbd683c0012199
Abdullah Shahid <67156544+Volcie@users.noreply.github.com>
2022-11-09 13:22:17 +0300

    added 'UsersService.fetchUserByUsername' func

--- src/modules/users/services/users.service.ts
+++ src/modules/users/services/users.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from "@nestjs/common";
 import { User } from "@prisma/client";
 import { PrismaService } from "@root/prisma.service";
+import * as bcrypt from "bcrypt";
 
 type CreateUserType = {
   username: string;
+  password: string;
   email: string;
   age: number;
 };
 
 export interface IUsersService {
   fetchUser(id: string): Promise<User | null>;
+  fetchUserByUsername(username: string): Promise<User | null>;
   createUser(data: CreateUserType): Promise<User>;
 }
 
@@ -17,13 +20,36 @@ export interface IUsersService {
 export class UsersService implements IUsersService {
   constructor(private readonly prisma: PrismaService) {}
   async fetchUser(id: string): Promise<User | null> {
-    return await this.prisma.user.findUnique({ where: { id } });
+    const user = await this.prisma.user.findUnique({ where: { id } });
+    delete user?.password;
+    delete user?.salt;
+    return user;
+  }
+  async fetchUserByUsername(username: string): Promise<User | null> {
+    const user = await this.prisma.user.findUnique({
+      where: { username },
+    });
+    delete user?.password;
+    delete user?.salt;
+    return user;
   }
 
-  async createUser({ username, email, age }: CreateUserType): Promise<User> {
+  async createUser({
+    username,
+    password,
+    email,
+    age,
+  }: CreateUserType): Promise<User> {
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+
     const user = await this.prisma.user.create({
-      data: { username, email, age },
+      data: { username, password: hashedPassword, salt, email, age },
     });
+
+    delete user.password;
+    delete user.salt;
+
     return user;
   }
 }
