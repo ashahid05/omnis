5a9640a20e707fec36f4f2805219ccdae5b557ab
Abdullah Shahid <67156544+Volcie@users.noreply.github.com>
2022-11-09 13:19:24 +0300

    updated prisma schema

---
+++ prisma/migrations/20221108180114_session/migration.sql
@@ -0,0 +1,37 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `userId` on the `Post` table. All the data in the column will be lost.
+  - Added the required column `authorId` to the `Post` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `content` to the `Post` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `title` to the `Post` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updated_at` to the `Post` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Post" DROP CONSTRAINT "Post_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "Post" DROP COLUMN "userId",
+ADD COLUMN     "authorId" TEXT NOT NULL,
+ADD COLUMN     "content" TEXT NOT NULL,
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "rating" TEXT,
+ADD COLUMN     "title" TEXT NOT NULL,
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL;
+
+-- CreateTable
+CREATE TABLE "Session" (
+    "id" TEXT NOT NULL,
+    "sid" TEXT NOT NULL,
+    "data" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Session_sid_key" ON "Session"("sid");
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
