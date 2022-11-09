be4ed6d95f045dc4c1b1140d000d3be4cef88762
Abdullah Shahid <67156544+Volcie@users.noreply.github.com>
2022-11-09 13:20:03 +0300

    added passport auth

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import { LoggingInterceptor } from "@interceptors/logging.interceptor";
 import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
-import { AppModule } from "./modules/app/app.module";
+import { AppModule } from "@app/app.module";
 import { PrismaService } from "./prisma.service";
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
 
 import * as session from "express-session";
+import * as passport from "passport";
+import { config } from "process";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -28,13 +31,25 @@ async function bootstrap() {
   }>("app.cors");
 
   app.enableCors(corsConfig);
+
   app.use(
     session({
+      name: configService.get<string>("app.cookie.name"),
+      store: new PrismaSessionStore(new PrismaService(), {
+        checkPeriod: 2 * 60 * 1000,
+        dbRecordIdIsSessionId: true,
+        dbRecordIdFunction: undefined,
+      }),
       secret: configService.get<string>("SESSION_SECRET"),
       saveUninitialized: false,
       resave: false,
+      cookie: {
+        maxAge: configService.get<number>("app.cookie.age"),
+      },
     }),
   );
+  app.use(passport.initialize());
+  app.use(passport.session());
 
   const port = configService.get<number>("app.port");
   await app.listen(port);
