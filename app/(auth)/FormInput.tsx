8bd4544e551124bc6a364099614a08bf1620414c
Abdullah Shahid <67156544+Volcie@users.noreply.github.com>
2022-11-12 15:24:54 +0300

    Added disabled functionality

--- app/(auth)/FormInput.tsx
+++ app/(auth)/FormInput.tsx
@@ -19,6 +19,7 @@ const FormInput: React.FC<FormInputProps> = ({
   error,
   icon,
   label,
+  disabled,
   ...props
 }) => {
   return (
@@ -35,9 +36,10 @@ const FormInput: React.FC<FormInputProps> = ({
       <div className="relative bg-cool-gray-800 rounded-lg">
         <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
           <FontAwesomeIcon
+            aria-disabled={disabled}
             icon={icon}
             className={Utils.concat(
-              "w-5 h-5 ",
+              "w-5 h-5 aria-disabled:text-gray-500",
               error ? "text-red-400" : "text-gray-400"
             )}
             fill="currentColor"
@@ -45,8 +47,11 @@ const FormInput: React.FC<FormInputProps> = ({
         </div>
         <input
           {...props}
+          name={props.id}
+          disabled={disabled}
           className={Utils.concat(
             "border text-sm rounded-lg block w-full pl-10 p-2.5 bg-cool-gray-800 placeholder-gray-400 text-white outline-none",
+            "disabled:cursor-not-allowed disabled:bg-cool-gray-900 disabled:border-gray-700 disabled:text-gray-400",
             error
               ? "border-red-400"
               : "border-gray-600 focus:ring-primary-500 focus:border-primary-500"
