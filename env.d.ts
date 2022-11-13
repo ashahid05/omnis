77706d841012ff7d0cbde8e093c0bd5852529468
Abdullah Shahid <67156544+Volcie@users.noreply.github.com>
2022-11-13 14:51:22 +0300

    Added fix for typescript error

--- env.d.ts
+++ env.d.ts
@@ -4,3 +4,9 @@ declare namespace NodeJS {
     NEXT_PUBLIC_API_ENDPOINT: string;
   }
 }
+
+declare module "sweetalert2/src/sweetalert2.js" {
+  export * from "sweetalert2";
+  // "export *" does not matches the default export, so do it explicitly.
+  export { default } from "sweetalert2";
+}
