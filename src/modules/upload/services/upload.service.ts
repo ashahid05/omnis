0cc40f090bf215861586a8e742566251b48f3877
Abdullah Shahid <67156544+Volcie@users.noreply.github.com>
2022-11-16 12:40:06 +0300

    Added method `UploadService.uploadImage`

--- src/modules/upload/services/upload.service.ts
+++ src/modules/upload/services/upload.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from "@nestjs/common";
+import { RpcException } from "@nestjs/microservices";
+import { S3 } from "aws-sdk";
+import * as crypto from "crypto";
+import { InjectAwsService } from "nest-aws-sdk";
 
 @Injectable()
-export class UploadService {}
+export class UploadService {
+  constructor(@InjectAwsService(S3) private readonly s3: S3) {}
+
+  async uploadImage(
+    body: Buffer,
+    metadata: Record<string, string>,
+    mimetype: string,
+    dest: string,
+  ) {
+    try {
+      const key = crypto.randomBytes(16).toString("hex");
+
+      const res = await this.s3
+        .putObject({
+          Bucket: "abdullah.gg",
+          Key: `omnis/${dest}/${key}`,
+          ContentType: mimetype,
+          Body: body,
+          Metadata: metadata,
+        })
+        .promise();
+
+      return { key, ...res };
+    } catch (error) {
+      throw new RpcException(error);
+    }
+  }
+}
