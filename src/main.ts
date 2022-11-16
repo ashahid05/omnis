097fd000d098a3e0d50b1db9d562f58a6c7abaff
Abdullah Shahid <67156544+Volcie@users.noreply.github.com>
2022-11-16 12:38:20 +0300

    Converted to TCP transport

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
-      transport: Transport.MQTT,
-      options: {
-        url: "mqtt://localhost:1883",
-      },
+      transport: Transport.TCP,
     },
   );
 
