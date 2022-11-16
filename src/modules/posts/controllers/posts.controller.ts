dc45c85dfcbfb507aed0ee17316f89a0c39f138a
Abdullah Shahid <67156544+Volcie@users.noreply.github.com>
2022-11-16 12:08:36 +0300

    Added route: `POST /posts/upload`

--- src/modules/posts/controllers/posts.controller.ts
+++ src/modules/posts/controllers/posts.controller.ts
@@ -3,14 +3,20 @@ import { AuthenticatedGuard } from "@modules/auth/utils/local.guard";
 import {
   Body,
   Controller,
+  FileTypeValidator,
   Get,
   HttpCode,
   HttpStatus,
+  MaxFileSizeValidator,
   Param,
+  ParseFilePipe,
   ParseIntPipe,
   Post,
+  UploadedFile,
   UseGuards,
+  UseInterceptors,
 } from "@nestjs/common";
+import { FileInterceptor } from "@nestjs/platform-express";
 import { CreatePostDto } from "../dtos/CreatePost.dto";
 import { PostsService } from "../services/posts.service";
 
@@ -38,5 +44,31 @@ export class PostsController {
     const post = await this.service.createPost({ ...data, author_id: userID });
 
     return post;
+  }
+
+  @Post("upload")
+  @UseGuards(AuthenticatedGuard)
+  @UseInterceptors(FileInterceptor("image"))
+  async uploadFile(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 1000 * 12000 }), // 1kb * 12 = 12MB
+          new FileTypeValidator({
+            fileType: /(image)\/(png|jpeg|avif|svg|webp+)/,
+          }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+    @User("id") ownerID: string,
+  ) {
+    const res = await this.service.uploadImage(
+      ownerID,
+      file.buffer,
+      file.mimetype,
+    );
+
+    return res;
   }
 }
