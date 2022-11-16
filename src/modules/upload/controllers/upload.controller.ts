6ce6742965615141c4ec26c9d6a87599e83f7a96
Abdullah Shahid <67156544+Volcie@users.noreply.github.com>
2022-11-16 12:39:26 +0300

    Added messagePattern: `{cmd: upload, dest: posts}`

--- src/modules/upload/controllers/upload.controller.ts
+++ src/modules/upload/controllers/upload.controller.ts
@@ -1,4 +1,26 @@
 import { Controller } from "@nestjs/common";
+import { MessagePattern, Payload } from "@nestjs/microservices";
+import { UploadFileDto } from "../dtos/UploadFile.dto";
+import { UploadService } from "../services/upload.service";
 
 @Controller()
-export class UploadController {}
+export class UploadController {
+  constructor(private readonly service: UploadService) {}
+
+  @MessagePattern({ cmd: "upload", dest: "posts" })
+  async handleFileUpload(@Payload() data: UploadFileDto) {
+    const { owner, body, mimetype } = data;
+
+    const res = await this.service.uploadFile(
+      Buffer.from(body, "base64"),
+      { owner },
+      mimetype,
+      "posts",
+    );
+
+    return {
+      success: true,
+      objectKey: res.key,
+    };
+  }
+}
