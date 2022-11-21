16ccb210e8f5d6d6b2e334d4963c21e613e50ad2
Abdullah Shahid <67156544+Volcie@users.noreply.github.com>
2022-11-21 16:14:25 +0300

    Added method: `Utils.createPost`

--- app/utils.ts
+++ app/utils.ts
@@ -5,6 +5,34 @@ namespace Utils {
     return [...items].join(" ").trim();
   }
 
+  export async function createPost(
+    title: string,
+    content: string,
+    image: File,
+    rating: string
+  ) {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_ENDPOINT}/posts/create`,
+      {
+        credentials: "include",
+        method: "POST",
+        body: JSON.stringify({
+          title,
+          content,
+          rating,
+          image,
+        }),
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+
+    const data = await res.json();
+
+    console.log(res);
+
+    return data;
+  }
+
   export async function fetchUser(): Promise<User | null | undefined> {
     const res = await fetch(
       `${process.env.NEXT_PUBLIC_API_ENDPOINT}/users/@me`,
