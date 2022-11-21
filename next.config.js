b8fcfb544cbceb86f2d4a32fd50c7edcb6edc55f
Abdullah Shahid <67156544+Volcie@users.noreply.github.com>
2022-11-21 16:09:31 +0300

    Added image storage host: 's3 amazon'

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "s3.eu-central-1.amazonaws.com" },
+    ],
+  },
   experimental: { appDir: true },
 };
 
