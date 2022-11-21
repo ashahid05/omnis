dce9fcca7f0344929ffffd8aff25251f76ed0f26
Abdullah Shahid <67156544+Volcie@users.noreply.github.com>
2022-11-21 16:11:57 +0300

    Changed ui for `/posts/[postId]`

--- app/(app)/posts/[postId]/Post.tsx
+++ app/(app)/posts/[postId]/Post.tsx
@@ -1,66 +1,78 @@
-import React from "react";
-import { Post } from "#types";
-import Link from "next/link";
+import { Post } from "@app/types";
+import moment from "moment";
 import Image from "next/image";
+import xss from "xss";
 import DummyImage from "public/images/dummy-pfp.jpeg";
+import Link from "next/link";
 
 const PostUI: React.FC<Post> = ({
   rating,
   title,
   author,
+  image,
   content,
+  created_at,
   author_id,
 }) => {
   return (
-    <article className="max-w-2xl px-6 py-24 mx-auto space-y-12 bg-cool-gray-900 rounded-xl shadow-2xl text-gray-50">
-      <div className="w-full mx-auto space-y-4 text-center">
-        {rating && (
-          <p className="text-xs font-semibold tracking-wider uppercase">
-            #{rating}
-          </p>
-        )}
-        <h1 className="text-4xl font-bold leading-tight md:text-5xl">
-          {title}
-        </h1>
-        <p className="text-sm text-gray-400">
-          <span>
-            by{" "}
-            <Link
-              rel="noopener noreferrer"
-              href={`/users/${author_id}`}
-              target="_blank"
-              className="underline text-primary-400"
-            >
-              {author.name}
-            </Link>
-          </span>{" "}
-          on <time dateTime="2022-2-12">Feb 12th 2022</time>
-        </p>
-      </div>
-      <div className="text-gray-100">
-        <p>{content}</p>
-      </div>
-      <div className="pt-12 border-t border-gray-700">
-        <div className="flex flex-col space-y-4 md:space-y-0 md:space-x-6 md:flex-row">
-          <Image
-            src={DummyImage}
-            width={512}
-            height={512}
-            alt=""
-            className="self-center object-cover flex-shrink-0 w-24 h-24 border rounded-full md:justify-self-start bg-gray-500 border-gray-700"
-          />
-          <div className="flex flex-col">
-            <h4 className="text-lg font-semibold">{author.name}</h4>
-            <p className="text-gray-400">
-              Sed non nibh iaculis, posuere diam vitae, consectetur neque.
-              Integer velit ligula, semper sed nisl in, cursus commodo elit.
-              Pellentesque sit amet mi luctus ligula euismod lobortis ultricies
-              et nibh.
-            </p>
+    <>
+      <div className="mb-4 md:mb-0 w-full max-w-screen-md mx-auto relative h-96">
+        <div
+          className="absolute left-0 bottom-0 w-full h-full z-10 rounded-lg"
+          style={{
+            backgroundImage:
+              "linear-gradient(180deg,transparent,rgba(0,0,0,.7))",
+          }}
+        ></div>
+        <Image
+          src={`${process.env.NEXT_PUBLIC_STORAGE}/posts/${image}`}
+          width={1000}
+          height={256}
+          alt="Post image"
+          className="absolute left-0 top-0 w-full h-full z-0 object-cover rounded-lg"
+        />
+        <div className="p-4 absolute bottom-0 left-0 z-20">
+          {rating && (
+            <div className="inline-flex h-8 text-sm px-4 py-2 mb-2 uppercase cursor-default items-center justify-center w-auto rounded-full overflow-hidden bg-gradient-to-r from-primary-700 to-primary-500 text-white font-bold">
+              <span className="whitespace-nowrap overflow-hidden text-ellipsis">
+                {rating}
+              </span>
+            </div>
+          )}
+          <h2 className="text-4xl font-semibold text-gray-100 leading-tight">
+            {title}
+          </h2>
+          <div className="flex mt-3">
+            <Image
+              src={DummyImage}
+              width={512}
+              height={512}
+              alt="Author image"
+              className="h-10 w-10 rounded-full mr-2 object-cover"
+            />
+            <div>
+              <p className="font-semibold text-gray-200 text-sm hover:underline">
+                <Link
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  href={`/users/${author_id}`}
+                >
+                  {author.name}
+                </Link>
+              </p>
+              <p className="font-semibold text-gray-400 text-xs">
+                {moment(created_at).format("MMM D YYYY")}
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </article>
+
+      <div
+        className="mt-8 text-gray-200 max-w-screen-md mx-auto text-lg leading-relaxed bg-cool-gray-900 p-4 rounded-lg post-html-content"
+        dangerouslySetInnerHTML={{ __html: xss(content) }}
+      ></div>
+    </>
   );
 };
 
